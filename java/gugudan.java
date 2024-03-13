d644608c469e627387a177a778c01cdc34d0ec54
OH-YUNJU <bibi02180@naver.com>
2024-03-13 17:06:13 +0900

    gugudan

--- java/gugudan.java
+++ java/gugudan.java
@@ -11,7 +11,7 @@ public class gugudan {
                 System.exit(1);
             } else if (i > 1 && i <10) {
                 for(int j=1; j<10; j++) {
-                    System.out.println(i + "+" + j + " = " + i + j);
+                    System.out.println(i + "*" + j + " = " + i * j);
                 }
             } else {
                 System.out.println("Insert Number 2~9. Retype again~!!");
